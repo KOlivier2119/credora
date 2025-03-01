ef51e6b44443c5b749547fb30fb261e15a640977
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2025-03-01 21:26:19 +0200

    modifying the navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <nav className="w-full bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-14">
           {/* Logo Section */}
           <div className="flex-shrink-0 flex items-center">
             <Image
